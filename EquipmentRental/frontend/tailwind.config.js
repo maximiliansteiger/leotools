d10c3be1365810eeabb35c5d3ef8ee29a6e92324
HolzerTeresa <teresaholzer3@gmail.com>
2022-10-02 21:03:14 +0200

    added tailwind

---
+++ EquipmentRental/frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app/rental/rental.component.html", "./src/app/register/register.component.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
