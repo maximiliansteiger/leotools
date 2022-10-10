bd1ea5f8d792d88d742f6314784b4c49bdbe3547
Maximilian Steiger <maximiliansteiger15@gmail.com>
2022-10-10 11:05:31 +0200

    Reverting to the state of the project

--- EquipmentRental/frontend/src/app/app-routing.module.ts
+++ EquipmentRental/frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { RentalComponent } from './rental/rental.component';
 import {RegisterComponent} from "./register/register.component";
 import { DetailComponent } from './detail/detail.component';
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   {
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'detail',
     component: DetailComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
   {
     path: '**',
--- EquipmentRental/frontend/src/app/login/login.component.ts
+++ EquipmentRental/frontend/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.router.navigate(['#', {relativeTo: this.route}]);
   }
 
 }
