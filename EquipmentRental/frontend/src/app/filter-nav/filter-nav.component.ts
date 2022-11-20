e0b292a07f0f6bec4e9c9961b1f43c57f39f4039
HolzerTeresa <teresaholzer3@gmail.com>
2022-11-20 21:07:11 +0100

    Working in filter

--- EquipmentRental/frontend/src/app/filter-nav/filter-nav.component.ts
+++ EquipmentRental/frontend/src/app/filter-nav/filter-nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Equipment } from '../models/equipment';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-filter-nav',
@@ -11,11 +13,37 @@ export class FilterNavComponent implements OnInit {
     start: new FormControl<Date | null>(null),
     end: new FormControl<Date | null>(null),
   });
+  type?: any;
+  brand?: any;
+  typeFilter = ["Video", "Fotografie", "Audio"];
+  brandFilter= ["Canon", "Nikon", "Zoom"];
+  equipments?: Equipment[];
 
 
-  constructor() { }
+  constructor(private http: HttpService) { }
 
   ngOnInit(): void {
+    this.http.getAllEquipments().subscribe((data) => {
+      this.equipments = data;
+    })
+  }
+
+  refreshFilter(){
+    console.log(this.type + " "+ this.brand);
+    let equipmentFiltered = this.filterEquipment();
+  }
+
+  filterEquipment(){
+    let equipmentFiltered!: Equipment[];
+    if(this.brand != undefined){}
+    this.equipments?.forEach(e => {
+      if(e.EquipmentType = this.type){
+        equipmentFiltered.push(e);
+      }
+    }
+    );
+    return equipmentFiltered;
+    
   }
 
 }
