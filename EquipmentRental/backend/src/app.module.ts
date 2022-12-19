e7811a8088fc3e1b32ffd0182fd1df71cbaae94d
Maximilian Steiger <maximiliansteiger15@gmail.com>
2022-12-19 14:45:28 +0100

    add bookmark to db and backend

--- EquipmentRental/backend/src/app.module.ts
+++ EquipmentRental/backend/src/app.module.ts
@@ -8,9 +8,10 @@ import { ReservationModule } from './reservation/reservation.module';
 import { EquipmentTypeModule } from './equipment-type/equipment-type.module';
 import { StatusModule } from './status/status.module';
 import { EquipmentController } from './equipment/equipment.controller';
+import { BookmarksModule } from './bookmarks/bookmarks.module';
 
 @Module({
-  imports: [EquipmentModule, UserModule, RoleModule, ReservationModule, EquipmentTypeModule, StatusModule],
+  imports: [EquipmentModule, UserModule, RoleModule, ReservationModule, EquipmentTypeModule, StatusModule, BookmarksModule],
   controllers: [AppController],
   providers: [AppService],
 })
